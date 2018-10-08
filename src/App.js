f002f6d28e8d7d230195d6fb066befdd7c0bc657
이종 <whd9001@daum.net>
2018-10-08 16:18:46 +0900

    기

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyName from './MyName';
+import MyJong from './MyJong';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MyName name="리액트" />
     );
   }
 }
+
+class App2 extends Component {
+  render2() {
+    return (
+      <MyName name="리액트" />
+    );
+  }
+}
+
+
 
 export default App;
